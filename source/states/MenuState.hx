32da2fcc2dea4aa774a183e25375b969d8d71ec6
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-17 21:43:24 -0300

    Atualizar o MenuState.hx

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -7,6 +7,7 @@ import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
 import flixel.util.FlxColor;
+import audio.master.AudioMaster;
 
 class MenuState extends FlxState
 {
@@ -39,7 +40,7 @@ class MenuState extends FlxState
 			FlxTween.tween(item, {alpha: 1}, 0.5, {startDelay: 0.1 * i, ease: FlxEase.quadOut});
 		}
 
-		updateSelection(0);
+		updateSelection(0, false);
 	}
 
 	override function update(elapsed:Float):Void
@@ -53,9 +54,24 @@ class MenuState extends FlxState
 		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.SPACE || FlxG.keys.justPressed.Z)
 			confirmSelection();
 		#end
+
+		#if mobile
+		for (touch in FlxG.touches.justStarted())
+		{
+			var tapped = menuItems.members.filter(item -> item.overlapsPoint(touch.getWorldPosition()));
+			if (tapped.length > 0)
+			{
+				var idx = menuItems.members.indexOf(tapped[0]);
+				if (idx == curSelected)
+					confirmSelection();
+				else
+					updateSelection(idx - curSelected);
+			}
+		}
+		#end
 	}
 
-	function updateSelection(change:Int):Void
+	function updateSelection(change:Int, playSfx:Bool = true):Void
 	{
 		curSelected = (curSelected + change + ITEMS.length) % ITEMS.length;
 
@@ -67,14 +83,19 @@ class MenuState extends FlxState
 			item.size = size;
 			item.color = color;
 		});
+
+		if (playSfx && change != 0)
+			AudioMaster.playSound("menu_select", "sfx");
 	}
 
 	function confirmSelection():Void
 	{
+		AudioMaster.playSound("menu_confirm", "sfx");
+
 		switch (curSelected)
 		{
 			case 0: FlxG.switchState(new PlayState());
-			case 1: // TODO: open options
+			case 1:
 			case 2: openfl.system.System.exit(0);
 		}
 	}
